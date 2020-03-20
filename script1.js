cde09f01d82257f2b75b2c73747d43df072d103e
Anna <sas_kuz2000@mail.ru>
2020-03-20 20:23:25 +0300

    ex 2 рабочая версия ( без правок никиты)

--- script1.js
+++ script1.js
@@ -27,16 +27,21 @@ for(let i=0;i<count_quest;i++) {
 //let a = prompt("Введите обязательную статью расходов в этом месяце","");
 //let b = prompt("Во сколько обойдется?","");
 //appData.expenses.a=b;
-alert(`Ваш свободный бюджет составляет ${(appData.your_money - waste)} `);
+alert(`Ваш свободный бюджет составляет ${(appData.your_money - waste)/30} `);
  
-switch ((appData.your_money - waste)) {
+/*switch ((appData.your_money - waste)) {
     case((appData.your_money - waste)<100): console.log('низкий бюджет') ;
     break;
     case(((appData.your_money - waste)>100) && ((appData.your_money - waste)<1000)):console.log( 'средний бюджет' );
     break;
     case((appData.your_money - waste)>1000): console.log('тратьте сколько хотите') ;
     break;
-}
+}*/
+ if (((appData.your_money - waste)/30)<100 ){
+    console.log('низкий бюджет') ;   
+ }else if ((((appData.your_money - waste)/30)>=100 ) && (((appData.your_money - waste)/30)<1000 )) {
+    console.log( 'средний бюджет' );   
+ } else console.log( 'высокий бюджет' );  ;
 
 //поменять промпт на инпут в html --  не могу т.к не знаю как потом управлять этой переменной 
 //master -> (checkout -b 'имя ветки новой') 
