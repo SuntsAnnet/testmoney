8b2f8e5f58ff9153c3a231a898f46baf6474591a
Anna <sas_kuz2000@mail.ru>
2020-03-20 19:33:05 +0300

    ex 2 ( без изменений никиты)

--- script1.js
+++ script1.js
@@ -1,5 +1,5 @@
 'use strict';
-let money = prompt("Ваш месячный бюджет","");
+let money = +prompt("Ваш месячный бюджет","");
 console.log(money);
 let time = prompt("Введите дату в формате YYYY-MM-DD",'');
 console.log(time);
@@ -11,11 +11,32 @@ let appData = {
     income : [],
     savings : false
 };
-let a = prompt("Введите обязательную статью расходов в этом месяце","");
-let b = prompt("Во сколько обойдется?","");
-appData.expenses.a=b;
-alert(`Ваш бюджет составляет ${(appData.you_money -appData.expenses.a)/30} `);
+let count_quest = prompt ("Сколько у вас статей расхода?","");
+var waste =0 ;
+for(let i=0;i<count_quest;i++) {
 
+    let a = prompt("Введите обязательную статью расходов в этом месяце","");
+    let b = +prompt("Во сколько обойдется?","");
+       if ((typeof(a))==='string' && (typeof(a))!= null && a!= ''  && (typeof(b))!= null && b!= ''){
+        appData.expenses[a]=b;   
+        waste =  waste + b;
+       } else {
+           console.log ('некоректный ввод');
+       }
+}
+//let a = prompt("Введите обязательную статью расходов в этом месяце","");
+//let b = prompt("Во сколько обойдется?","");
+//appData.expenses.a=b;
+alert(`Ваш свободный бюджет составляет ${(appData.you_money - waste)} `);
+ 
+switch ((appData.you_money - waste)/30) {
+    case'((appData.you_money - waste)/30)<100': console.log('низкий бюджет') ;
+    break;
+    case'((appData.you_money - waste)/30)>100 && ((appData.you_money - waste)/30)<1000':console.log( 'средний бюджет' );
+    break;
+    case'((appData.you_money - waste)/30)>1000': console.log('тратьте сколько хотите') ;
+    break;
+}
 
-//поменять промпт на инпут в html
+//поменять промпт на инпут в html --  не могу т.к не знаю как потом управлять этой переменной 
 //master -> (checkout -b 'имя ветки новой') 
